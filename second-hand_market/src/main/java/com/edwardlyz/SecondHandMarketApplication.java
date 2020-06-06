996b044f746466e6d86716054b797d81040b9d74
Edward-lyz <65755881+Edward-lyz@users.noreply.github.com>
2020-06-06 16:11:10 +0800

    Add files via upload

--- second-hand_market/src/main/java/com/edwardlyz/SecondHandMarketApplication.java
+++ second-hand_market/src/main/java/com/edwardlyz/SecondHandMarketApplication.java
@@ -1,9 +1,11 @@
 package com.edwardlyz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com.edwardlyz")
+@MapperScan("com.edwardlyz.dao")
 public class SecondHandMarketApplication {
 
 	public static void main(String[] args) {
