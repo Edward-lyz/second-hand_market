c0bca89745bf3563c77b86516062b69fc90dba63
Edward-lyz <65755881+Edward-lyz@users.noreply.github.com>
2020-06-06 12:00:01 +0800

    Add files via upload

---
+++ second-hand_market/src/main/java/com/edwardlyz/SecondHandMarketApplication.java
@@ -0,0 +1,13 @@
+package com.edwardlyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondHandMarketApplication.class, args);
+	}
+
+}
---
+++ second-hand_market/src/test/java/com/edwardlyz/SecondHandMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwardlyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
