cebc5b68a5f8cc2e6146046423a9636c6a667e83
Edward-lyz <sonya24yr@gmail.com>
2020-06-17 20:52:26 +0800

    delete dupulicates

--- second-hand_market/target/classes/sql/example.sql
+++
@@ -1,8 +0,0 @@
-/*一个简单的sql语句，用来创建一个简易的user表*/
-CREATE TABLE `user` (
-  `id` int(13) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(33) DEFAULT NULL COMMENT '姓名',
-  `age` int(3) DEFAULT NULL COMMENT '年龄',
-  `money` double DEFAULT NULL COMMENT '账户余额',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
